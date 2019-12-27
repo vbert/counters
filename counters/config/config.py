3a5fa5cda753e6a6b56f87f9892002b02a87803d
Wojciech Sobczak <wojciech.sobczak@petrus.pl>
2019-12-27 15:27:32 +0100

    Additional output file

--- counters/config/config.py
+++ counters/config/config.py
@@ -3,21 +3,81 @@
 # 2019-11
 import os
 
+APP_WINDOW_FILE = 'appwindow.ui'
+
 DEFAULT_COUNTERS = 'liczniki.dat'
 DEFAULT_PATHS = 'sciezki.dat'
 TMP_INPUT_FILE = 'tmp-src-utf8.txt'
 
-INPUT_DELIMITER = '|'
-INPUT_COLUMNS = ['Data', 'Wejście dod 1', 'Energia', 'Adres']
-COL_DATE_READING = 0
-COL_METER_READING_1 = 1
-COL_METER_READING_2 = 2
-COL_ADDRESS = 3
-OUTPUT_FILE = 'notesrecrs.sod'
-OUTPUT_TPL_START = 'START ODCZYTY;'
-OUTPUT_TPL_ROW = ':{place_index},1,{date_reading},2,,,,1,{meter_reading_1},0,2,{meter_reading_2},0;'
-OUTPUT_TPL_END = 'KONIEC ODCZYTY;'
-APP_WINDOW_FILE = 'appwindow.ui'
+# Conversion parameters
+CONVERSION_PARAMETERS = {
+    'rental': {
+        'delimiter': '|',
+        'columns': {
+            'names': ['Data', 'Wejście dod 1', 'Energia', 'Adres'],
+            'indexes': {
+                'date': 0,
+                'reading_1': 1,
+                'reading_2': 2,
+                'address': 3
+            }
+        },
+        'file_name': 'notesrecrs.sod',
+        'template': {
+            'start': 'START ODCZYTY;',
+            'address_as': 'index',
+            'row': ':{place_index},1,{date_reading},2,,,,1,{meter_reading_1},0,2,{meter_reading_2},0;',
+            'end': 'KONIEC ODCZYTY;'
+        }
+    },
+    'analysis': {
+        'delimiter': '|',
+        'columns': {
+            'names': ['Adres', 'Data', 'Energia', 'Wejście dod 1', 'Wejście dod 2', 'Wejście dod 3', 'Wejście dod 4'],
+            'indexes': {
+                'address': 0,
+                'date': 1,
+                'energy': 2,
+                'reading_1': 3,
+                'reading_2': 4,
+                'reading_3': 5,
+                'reading_4': 6
+            }
+        },
+        'part_path': 'excel',
+        'template': {
+            'start': 'Adres,Data,Energia,Wejście dod 1,Wejście dod 2,Wejście dod 3,Wejście dod 4',
+            'address_as': 'string',
+            'row': '{place_address},{date_reading},{energy},{meter_reading_1},{meter_reading_2},{meter_reading_3},{meter_reading_4}',
+            'end': ''
+        }
+    },
+}
+# Rental
+# INPUT_RENTAL_DELIMITER = '|'
+# INPUT_RENTAL_COLUMNS = ['Data', 'Wejście dod 1', 'Energia', 'Adres']
+# COL_RENTAL_DATE_READING = 0
+# COL_RENTAL_METER_READING_1 = 1
+# COL_RENTAL_METER_READING_2 = 2
+# COL_RENTAL_ADDRESS = 3
+# OUTPUT_RENTAL_FILE = 'notesrecrs.sod'
+# OUTPUT_RENTAL_TPL_START = 'START ODCZYTY;'
+# OUTPUT_RENTAL_TPL_ROW = ':{place_index},1,{date_reading},2,,,,1,{meter_reading_1},0,2,{meter_reading_2},0;'
+# OUTPUT_RENTAL_TPL_END = 'KONIEC ODCZYTY;'
+
+# Analysis
+# INPUT_ANALYSIS_DELIMITER = '|'
+# INPUT_ANALYSIS_COLUMNS = ['Adres', 'Data', 'Energia', 'Wejście dod 1', 'Wejście dod 2', 'Wejście dod 3', 'Wejście dod 4']
+# COL_ANALYSIS_ADDRESS = 0
+# COL_ANALYSIS_DATE_READING = 1
+# COL_ANALYSIS_ENERGY = 2
+# COL_ANALYSIS_METER_READING_1 = 3
+# COL_ANALYSIS_METER_READING_2 = 4
+# COL_ANALYSIS_METER_READING_3 = 5
+# COL_ANALYSIS_METER_READING_4 = 6
+# OUTPUT_ANALYSIS_PART_PATH = 'excel'
+# OUTPUT_ANALYSIS_TPL_START = 'Adres,Data,Energia,Wejście dod 1,Wejście dod 2,Wejście dod 3,Wejście dod 4'
+# OUTPUT_ANALYSIS_TPL_ROW = '{place_address},{date_reading},{energy},{meter_reading_1},{meter_reading_2},{meter_reading_3},{meter_reading_4}'
 
 
 def test():
--- counters/lib/tools.py
+++ counters/lib/tools.py
@@ -35,7 +35,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         # Readings files view
         self.readings = get_readings_files()
-        # ['Styczeń 2019.txt', 'Luty 2019.txt', 'Marzec 2019.txt', 'Kwiecień 2019 dodatek.txt']
         self.model = QtGui.QStandardItemModel()
         for reading in self.readings:
             self.model.appendRow(QtGui.QStandardItem(reading))
@@ -45,7 +44,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # Export button
         self.file_name = self.readings[self.file_index]
         self.exportButton.clicked.connect(self.convert)
-        #lambda: run_convert(self.file_name)
 
         # Close button
         self.closeButton.clicked.connect(self.close_app)
@@ -73,33 +71,39 @@ def run():
 
 
 def run_convert(src_file):
-    #src_file = 'Wielewska-18-08-28 07m11s57 format.txt'
     src_path = get_readings_path()
+    out_path = get_output_path()
     input_file = os.path.join(src_path, src_file)
     input_file_utf8 = os.path.join(TMP_PATH, conf.TMP_INPUT_FILE)
     decode_readings(input_file, input_file_utf8)
 
-    # generate csv file for future analysis
-    print(src_file)
-    csv_out_name = os.path.splitext(src_file)[0]
-    print(csv_out_name)
-    csv_out_file_name = '.'.join([os.path.splitext(src_file)[0], 'csv'])
-    print(csv_out_file_name)
+    # For rental
+    rental_readings = get_readings(input_file_utf8, conf.CONVERSION_PARAMETERS['rental']['columns']['names'])
+    rental_output_file = os.path.join(out_path, conf.CONVERSION_PARAMETERS['rental']['file_name'])
+    
+    # For analysis
+    analysis_readings = get_readings(input_file_utf8, conf.CONVERSION_PARAMETERS['analysis']['columns']['names'])
+    analysis_output_file = os.path.join(src_path, conf.CONVERSION_PARAMETERS['analysis']['part_path'], '.'.join([os.path.splitext(src_file)[0], 'csv']))
+    
+    if rental_readings and analysis_readings:
+        rental_contents = convert_for(rental_readings, conf.CONVERSION_PARAMETERS['rental'])
+        analysis_contents = convert_for(analysis_readings, conf.CONVERSION_PARAMETERS['analysis'])
 
-    readings = get_readings(input_file_utf8)
-    if readings:
-        contents = convert(readings)
-        out_path = get_output_path()
-        out_file = conf.OUTPUT_FILE
+        out_file = conf.OUTPUT_RENTAL_FILE
         output_file = os.path.join(out_path, out_file)
         out = open(output_file, mode='w', encoding='UTF-8')
-        contents = "\n".join(contents)
-        out.write(contents)
+        rental_contents = "\n".join(rental_contents)
+        out.write(rental_contents)
         out.close()
+        
         message = 'Konwersja zakończona pomyślnie.'
     else:
         message = 'Konwersja nie powiodła się.'
     return message
+
+
+def save_output(full_path, contents):
+    pass
 
 
 def get_paths(file_name):
@@ -111,16 +115,16 @@ def get_paths(file_name):
         return False
 
 
-def get_readings_path(file_name=conf.DEFAULT_PATHS):
-    paths = get_paths(file_name)
-    return paths[0].strip()
-
-
 def get_readings_files():
     readings_path = get_readings_path()
     files_list = [f for f in os.listdir(readings_path) if os.path.isfile(
         os.path.join(readings_path, f)) and f.endswith('.txt')]
     return sorted(files_list, key=str.casefold, reverse=True)
+
+
+def get_readings_path(file_name=conf.DEFAULT_PATHS):
+    paths = get_paths(file_name)
+    return paths[0].strip()
 
 
 def get_output_path(file_name=conf.DEFAULT_PATHS):
@@ -143,19 +147,19 @@ def decode_readings(input_file, output_file):
 
 
 def strip_csv_row(row):
-    row = list(map(str.strip, row.split(conf.INPUT_DELIMITER)))
-    return conf.INPUT_DELIMITER.join(row)
+    row = list(map(str.strip, row.split(conf.INPUT_RENTAL_DELIMITER)))
+    return conf.INPUT_RENTAL_DELIMITER.join(row)
 
 
-def get_readings(file):
+def get_readings(file, columns_list):
     if os.path.isfile(file):
         import csv
         readings = []
         with open(file, encoding='UTF-8') as csvfile:
-            reader = csv.DictReader(csvfile, delimiter=conf.INPUT_DELIMITER)
+            reader = csv.DictReader(csvfile, delimiter=conf.INPUT_RENTAL_DELIMITER)
             for line in reader:
                 row = {}
-                for col in conf.INPUT_COLUMNS:
+                for col in columns_list:
                     row[col] = line[col]
                 readings.append(row)
         return readings
@@ -163,24 +167,33 @@ def get_readings(file):
         return False
 
 
-def convert(readings):
+def convert_for(readings, what):
+    if what == 'rental':
+        return convert_for_rental(readings, conf.CONVERSION_PARAMETERS['rental'])
+
+
+def convert_for_rental(readings, parameters):
     contents = []
-    contents.append(conf.OUTPUT_TPL_START)
+    contents.append(parameters['template']['start'])
     places_ids = get_places_ids()
     for line in readings:
-        address = line[conf.INPUT_COLUMNS[conf.COL_ADDRESS]]
+        address = line[parameters['columns']['indexes']['address']]
         if address in places_ids:
             args = {
                 'place_index': places_ids[address],
-                'date_reading': format_date(line[conf.INPUT_COLUMNS[conf.COL_DATE_READING]]),
-                'meter_reading_1': format_meter(line[conf.INPUT_COLUMNS[conf.COL_METER_READING_1]]),
-                'meter_reading_2': format_meter(line[conf.INPUT_COLUMNS[conf.COL_METER_READING_2]])
+                'date_reading': format_date(line[parameters['columns']['indexes']['date']]),
+                'meter_reading_1': format_meter(line[parameters['columns']['indexes']['reading_1']]),
+                'meter_reading_2': format_meter(line[parameters['columns']['indexes']['reading_2']])
             }
-            tpl = conf.OUTPUT_TPL_ROW
+            tpl = parameters['template']['row']
             row = tpl.format(**args)
             contents.append(row)
-    contents.append(conf.OUTPUT_TPL_END + "\n")
+    contents.append(parameters['template']['end'] + "\n")
     return contents
+
+
+def convert_for_analysis(readings):
+    pass
 
 
 def get_places_ids(file_name=conf.DEFAULT_COUNTERS):
